165dcd66d0e4c69da70998c516e5276e8059a833
RohanKamat14 <94646580+RohanKamat14@users.noreply.github.com>
2024-07-28 15:39:33 -0500

    Add files via upload

---
+++ mysite/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.template import loader
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
+
+def courses(request):
+    template = loader.get_template('courses.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
