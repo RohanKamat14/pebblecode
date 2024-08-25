da412ddc0595696399a51dd7c42da120d6cc2b7c
BrandonSwole <b.swole1213@gmail.com>
2024-08-25 14:46:24 -0600

    User Authentication/Login 1

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("courses", views.courses, name='courses'),
-    path("login", views.login, name='login'),
+    path("login", views.login_view, name='login'),
     path("Signup", views.Signup, name = 'Signup')
 ]
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 def index(request):
     template = loader.get_template('index.html')
@@ -18,21 +19,25 @@ def courses(request):
     }
     return HttpResponse(template.render(context, request))
 
-def login(request):
-    template = loader.get_template('login.html')
-    context = {
-
-    }
-    return HttpResponse(template.render(context, request))
-
 def Signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            auth_login(request, form.save())
             return redirect("index")  
     else:
         form = UserCreationForm()  
 
     # Render the template with the form
     return render(request, "Signup.html", {"form": form })
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #Login here
+            auth_login(request, form.get_user())
+            return redirect("index")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form })
