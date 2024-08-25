e32c710ad7ca476a5875e8fe12c4d2a375dbd75d
BrandonSwole <b.swole1213@gmail.com>
2024-08-25 13:35:55 -0600

    User Registration 1

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     template = loader.get_template('index.html')
@@ -24,9 +24,15 @@ def login(request):
 
     }
     return HttpResponse(template.render(context, request))
-def Signup(request):
-    template = loader.get_template('Signup.html')
-    context = {
 
-    }
-    return HttpResponse(template.render(context, request))
+def Signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")  
+    else:
+        form = UserCreationForm()  
+
+    # Render the template with the form
+    return render(request, "Signup.html", {"form": form })
