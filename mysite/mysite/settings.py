835b1ced0866d53237dcc2a83eaacdbb0e707691
RohanKamat14 <rohannkamat@gmail.com>
2024-08-27 21:12:37 -0500

    hi

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#Add this in your settings.py file:
 
 # Application definition
 
@@ -117,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    "/home/static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
